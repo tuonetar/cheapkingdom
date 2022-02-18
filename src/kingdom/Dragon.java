648386100f4e4fe525e241c77f5491f2882ab6fc
tuonetar <karina.martynava@gmail.com>
2022-02-18 16:37:22 +0100

    FLying Interface

--- src/kingdom/Dragon.java
+++ src/kingdom/Dragon.java
@@ -2,4 +2,16 @@ package kingdom;
 
 public class Dragon extends Animal implements Flying {
 
+	@Override
+	public boolean takeOff() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean land() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
 }
