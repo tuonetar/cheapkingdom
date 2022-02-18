660c97eefb0d393a3d6e3d8a3894ce23471e51d2
Sylvain Yvonneau <yvonneau.sylvain@gmail.com>
2022-02-18 16:36:31 +0100

    SpacialElement jusqu'à MovableBeing

--- src/kingdom/MovableBeing.java
+++ src/kingdom/MovableBeing.java
@@ -2,4 +2,56 @@ package kingdom;
 
 public abstract class MovableBeing extends Being implements Movable, Attacker {
 
+	protected double maxSpeed;
+	protected int force;
+	protected boolean inAir;
+	
+	public MovableBeing(String name, int x, int y, double maxHealth, double health, double resistance, double maxSpeed,
+			int force, boolean inAir) {
+		super(name, x, y, maxHealth, health, resistance);
+		this.maxSpeed = maxSpeed;
+		this.force = force;
+		this.inAir = inAir;
+	}
+	
+	public abstract void communicate();
+
+	public double getMaxSpeed() {
+		return maxSpeed;
+	}
+
+	public void setMaxSpeed(double maxSpeed) {
+		this.maxSpeed = maxSpeed;
+	}
+
+	public int getForce() {
+		return force;
+	}
+
+	public void setForce(int force) {
+		this.force = force;
+	}
+
+	public boolean isInAir() {
+		return inAir;
+	}
+
+	public void setInAir(boolean inAir) {
+		this.inAir = inAir;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("MovableBeing [maxSpeed=");
+		builder.append(maxSpeed);
+		builder.append(", force=");
+		builder.append(force);
+		builder.append(", inAir=");
+		builder.append(inAir);
+		builder.append("]");
+		return builder.toString();
+	}
+	
+	
 }
--- src/kingdom/Tree.java
+++ src/kingdom/Tree.java
@@ -2,4 +2,9 @@ package kingdom;
 
 public class Tree extends Being {
 
+	public Tree(String name, int x, int y, double maxHealth, double health, double resistance) {
+		super(name, x, y, maxHealth, health, resistance);
+		// TODO Auto-generated constructor stub
+	}
+
 }
